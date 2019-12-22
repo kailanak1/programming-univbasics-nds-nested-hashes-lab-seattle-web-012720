43e7d57de010a8a098bc6a1a3124e120fdd4aeee
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-22 04:20:45 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -10,7 +10,8 @@ def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
   infinite_jest = [
-    {name =>}]
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name}]
 end
 
 def literal_aoh
